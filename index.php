b9ee4e1d70289a812edb335c581e2f8e914eb566
yizhizoe <gao_yizhi@126.com>
2018-06-06 10:56:49 +0800

    v2

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 </head>
 <body>
   <div align="center">
-    <h1>AWS DevOps Demo - V1</h1>
+    <h1>AWS DevOps Demo - V2</h1>
     <h2>Github + ALB + CodeDeploy + Jenkins</h2> 
     <h2>AWS BJS CodeDeploy Demo</h2>
   </div>
