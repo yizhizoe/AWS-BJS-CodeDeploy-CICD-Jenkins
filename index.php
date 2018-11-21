2fbc75359e7337286056a2ae59f5e5e87565e452
yizhizoe <gao_yizhi@126.com>
2018-11-21 10:58:45 +0800

    V5

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 </head>
 <body>
   <div align="center">
-    <h1>AWS DevOps Demo - V2</h1>
+    <h1>AWS DevOps Demo - V5</h1>
     <h2>Github + ALB + CodeDeploy + Jenkins</h2> 
     <h2>Cross-account CodeDeploy Demo</h2>
   </div>
